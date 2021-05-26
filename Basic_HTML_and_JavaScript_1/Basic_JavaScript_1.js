ab796f4a19ce855116333ae79b14df9db91c3998
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-05-26 12:22:12 -0700

    Update Basic_JavaScript_1.js

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,1 +1,7 @@
 alert("Hello, World! This is a JavaScript Alert!");
+function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.frontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+		}
